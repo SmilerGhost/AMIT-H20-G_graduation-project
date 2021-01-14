4902405c3d1012dd9b4eed6975a65b7102f92df1
SmilerGhost <72670509+SmilerGhost@users.noreply.github.com>
2021-01-14 12:17:45 +0200

    Update main.c
    1

--- master ECU/AMIT-GP/AMIT-GP/main.c
+++ master ECU/AMIT-GP/AMIT-GP/main.c
@@ -7,6 +7,11 @@
 
 #include "SPI.h"
 #include "USART_RS232_H_file.h"
+
+#define RECEIVE_FROM_USART_SEND_VIA_SPI \
+ORDER = USART_RxChar(); \
+SPI_SendByte(ORDER);
+
 int main(void)
 {
 	USART_Init(9600);
@@ -14,8 +19,7 @@ int main(void)
 	Uint8t ORDER = 0;
 	while (1)
 	{
-		ORDER = USART_RxChar();
-		SPI_SendByte(ORDER);
+		RECEIVE_FROM_USART_SEND_VIA_SPI
 	}
 }
 
